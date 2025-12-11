1a701447f24f07b836992ad6e4974d0e7b1ede06
ErikaSousa <erika.docus@gmail.com>
2025-12-11 08:41:27 +0100

    feat(comprobarCodigoValido): Comprobar que el código de las fichas que inserte el usuario sea válido

---
+++ src/comprobarCodigoValido.py
@@ -0,0 +1,10 @@
+colorPermitido = {"RED", "GREEN", "BLUE", "YELLOW"}
+
+def codigoValido(codigo):
+    if len(codigo) != 4:
+        return False
+    
+    for color in codigo:
+        if color not in colorPermitido:
+            return False
+    return True
