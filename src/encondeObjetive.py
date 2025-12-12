6df5cd0cc4896ce1726be9459ea2d229ab8a8f07
XianGomez <xiiangomezz@gmail.com>
2025-12-12 13:57:17 +0100

    refactor(numeroFichas): Refactorizar valor asignado a numero fichas

---
+++ src/encondeObjetive.py
@@ -0,0 +1,40 @@
+import random
+from definirParametros import definirParametros
+
+fichas = ["RED", "BLUE", "YELLOW", "GREEN"]
+codigoObjetivo = []
+
+def generarCodigoAutomatico(codigoObjetivo, fichas, n=definirParametros()['LongitudCodigo']):
+    if n > 4:
+        raise ValueError("El número de fichas no puede ser mayor a 4")
+    for _ in range(n):
+        codigoObjetivo.append(random.choice(fichas))
+
+    print("Código: ", codigoObjetivo)
+    return codigoObjetivo
+
+def generarCodigoManual(codigoObjetivo, n=definirParametros()['LongitudCodigo']):
+    if n > 4:
+        raise ValueError("El número de fichas no puede ser mayor a 4")
+    for i in range(n):
+        ficha = input(f"Introduce el color de la ficha {i+1} (RED, BLUE, YELLOW, GREEN): ").upper()
+        while ficha not in fichas:
+            print("Color no válido. Inténtalo de nuevo.")
+            ficha = input(f"Introduce el color de la ficha {i+1} (RED, BLUE, YELLOW, GREEN): ").upper()
+        codigoObjetivo.append(ficha)
+
+    print("Código: ", codigoObjetivo)
+    return codigoObjetivo
+
+def elegirMetodoGeneracion():
+    opcion = input("¿Quieres generar el código manualmente (M) o automáticamente (A)? ").upper()
+    if opcion == 'M':
+        generarCodigoManual(codigoObjetivo)
+    elif opcion == 'A':
+        generarCodigoAutomatico(codigoObjetivo, fichas)
+    else:
+        print("Opción no válida.")
+
+
+elegirMetodoGeneracion()
+
