7d5e2126e1d00ace9f16fca9086461b45f465b7b
ErikaSousa <erika.docus@gmail.com>
2025-12-12 13:20:35 +0100

    feat(definirParametros): Definir los parametros del algoritmo

--- src/definirParametros.py
+++ src/definirParametros.py
@@ -5,8 +5,7 @@ def definirParametros():
 
     parametros['tamanhoPoblacion'] = 100
     parametros['maxGeneraciones'] = 14
-    parametros['OperadorSeleccion'] = random.choice ("rango", "ruleta", "torneo")
+    parametros['OperadorSeleccion'] = random.choice(["rango", "ruleta", "torneo"])
     parametros['LongitudCodigo'] = 4
 
     return parametros
-
