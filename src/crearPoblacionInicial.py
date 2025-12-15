be0c5c4358f659bfec1ba37f6f885af5ac75c534
XianGomez <xiiangomezz@gmail.com>
2025-12-15 13:33:20 +0100

    feat(crearPoblacionInicial): Añadir la creación de la población inicial

---
+++ src/crearPoblacionInicial.py
@@ -0,0 +1,27 @@
+import random
+from definirParametros import definirParametros
+
+
+fichas = ["RED", "BLUE", "YELLOW", "GREEN"]
+poblacionInicial = []
+
+def crearPoblacionInicial(poblacionMaxima):
+    
+
+    while len(poblacionInicial) < poblacionMaxima:
+
+        nuevoIndividuo = []
+        for _ in range(0,4):
+            fichaAleatoria = random.choice(fichas)
+            nuevoIndividuo.append(fichaAleatoria)
+        
+        poblacionInicial.append(nuevoIndividuo)
+
+    return poblacionInicial
+
+
+    
+    
+            
+
+crearPoblacionInicial(poblacionMaxima=definirParametros()['tamanhoPoblacion'])
