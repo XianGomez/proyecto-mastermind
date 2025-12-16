200c3928c79eb220b5d7ac2ca3feae7cb8c3026c
ErikaSousa <erika.docus@gmail.com>
2025-12-16 16:25:34 +0100

    fix(evaluarIndividuos): Quitar código innecesario

--- src/evaluarIndividuos.py
+++ src/evaluarIndividuos.py
@@ -1,8 +1,9 @@
 def evaluarIndividuos(individuos, codigoObjetivo):
     resultados = []
     for individuo in individuos:
-        aciertos = sum(1 for i in range(len(codigoObjetivo)) if individuo[i] == codigoObjetivo[i])
-        resultados.append((individuo, aciertos))
+        aciertos = sum(
+            1 for i in range(len(codigoObjetivo)) 
+            if individuo[i] == codigoObjetivo[i])
     return resultados
 
              
