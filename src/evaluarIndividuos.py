9fd62abb8078640c1ddc146aae9361d7691a12b2
ErikaSousa <erika.docus@gmail.com>
2025-12-16 16:31:55 +0100

    refactor(evaluarIndividuos): Refactorizar el nombre de una variable

--- src/evaluarIndividuos.py
+++ src/evaluarIndividuos.py
@@ -1,6 +1,6 @@
-def evaluarIndividuos(individuos, codigoObjetivo):
+def evaluarIndividuos(poblacionInicial, codigoObjetivo):
     resultados = []
-    for individuo in individuos:
+    for individuo in poblacionInicial:
         aciertos = sum(
             1 for i in range(len(codigoObjetivo)) 
             if individuo[i] == codigoObjetivo[i])
