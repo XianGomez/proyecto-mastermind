e70beb77a18551259e6cd6318cc3293ea47f2325
XianGomez <xiiangomezz@gmail.com>
2025-12-17 12:15:23 +0100

    refactor(crearPoblacionIncial): Cambiar valor asignado a constantes

--- src/crearPoblacionInicial.py
+++ src/crearPoblacionInicial.py
@@ -1,14 +1,13 @@
 import random
-from definirParametros import definirParametros
-
+import parametros 
+tamanhoPoblacion, maxGeneraciones, operadorSeleccion, longitudCodigo, tasaMutacion = parametros.parametros()
 
 fichas = ["RED", "BLUE", "YELLOW", "GREEN", "ORANGE", "PURPLE"]
 poblacionInicial = []
 
-def crearPoblacionInicial(poblacionMaxima):
-    
+def crearPoblacionInicial(TAMANHOPOBLACION=tamanhoPoblacion):
 
-    while len(poblacionInicial) < poblacionMaxima:
+    while len(poblacionInicial) < TAMANHOPOBLACION:
 
         nuevoIndividuo = []
         for _ in range(0,4):
@@ -18,10 +17,3 @@ def crearPoblacionInicial(poblacionMaxima):
         poblacionInicial.append(nuevoIndividuo)
 
     return poblacionInicial
-
-
-    
-    
-            
-
-crearPoblacionInicial(poblacionMaxima=definirParametros()['tamanhoPoblacion'])
