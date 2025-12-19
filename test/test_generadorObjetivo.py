3699ff746f49a8ead38a5ac8644932ec358d08c2
la.rizos <erika.docus@gmail.com>
2025-12-19 12:36:24 +0100

    Delete test/test_generadorObjetivo.py

--- test/test_generadorObjetivo.py
+++
@@ -1,7 +0,0 @@
-import random
-from encondeObjetive import generarCodigoAutomatico, fichas
-
-def test_codigo_automatico():
-    test_codigo = []
-    resultado = generarCodigoAutomatico(test_codigo, fichas)
-    assert len(resultado) == 4
