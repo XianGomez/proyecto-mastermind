4752033e51016c8c72f2c77c895d695fd0dad58f
la.rizos <erika.docus@gmail.com>
2025-12-19 12:30:26 +0100

    Delete test/test_generadorObjetivo.py

--- test/test_generadorObjetivo.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from src.generarCodigoSecreto import generarCodigoAutomatico, fichas
-
-def test_generarCodigoAutomatico_length(monkeypatch):
-    test_codigo = []
-    monkeypatch.setattr('random.choice', lambda x: "RED")
-    resultado = generarCodigoAutomatico(test_codigo, fichas)
-    assert len(resultado) == 4  
