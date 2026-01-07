b574720b5311dfc1fbcaec50d3ea8858690e4c0f
XianGomez <xiiangomezz@gmail.com>
2026-01-07 15:52:49 +0100

    feat(main): Graficar mastermind

--- src/main.py
+++ src/main.py
@@ -5,32 +5,28 @@ from evaluarIndividuos import evaluarIndividuos
 from seleccionarPadres import seleccionarPadres
 from reproduccion import reproduccion
 from poblarGeneracion import poblarGeneracion
-
+from visualizacion import graficar_mastermind 
 
 tamanhoPoblacion, maxGeneraciones, operadorSeleccion, longitudCodigo, tasaMutacion, tasaCruce = parametros()
-codigoObjetivo = generarCodigoAutomatico(fichas, longitudCodigo) 
-
+codigoObjetivo = generarCodigoAutomatico(fichas, longitudCodigo)
 poblacion = crearPoblacionInicial()
 
-print(f"Objetivo a encontrar: {codigoObjetivo}")
+historial_mejores = [] 
 
 for generacion in range(maxGeneraciones):
-    
     puntuaciones = evaluarIndividuos(poblacion, codigoObjetivo)
     
-    if max(puntuaciones) == longitudCodigo:
-        indiceGanador = puntuaciones.index(longitudCodigo)
-        print(f"--- ¡SOLUCIÓN ENCONTRADA en Gen {generacion}! ---")
-        print(f"Código: {poblacion[indiceGanador]}")
+    mejor_puntuacion = max(puntuaciones)
+    mejor_individuo = poblacion[puntuaciones.index(mejor_puntuacion)]
+    historial_mejores.append(mejor_individuo)
+    
+    if mejor_puntuacion == longitudCodigo:
+        print(f"¡Solución encontrada en Gen {generacion}!")
         break
     
     padres = seleccionarPadres(poblacion, puntuaciones)
-    
     hijos = reproduccion(padres, tasaMutacion, tasaCruce)
-    
     poblacion = poblarGeneracion(hijos, tamanhoPoblacion)
-    
-    print(f"Generación {generacion} | Mejor puntuación: {max(puntuaciones)}")
 
-else:
-    print("Se alcanzó el máximo de generaciones sin encontrar la solución exacta.")
+graficar_mastermind(historial_mejores, codigoObjetivo)
+
