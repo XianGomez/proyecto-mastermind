a4dfde3f075f18b4fd2ef4993184110ef1e01399
XianGomez <xiiangomezz@gmail.com>
2026-01-08 09:26:36 +0100

    fix(tests): Añadir linea para que encuentren la carpeta src
    Add sys.path manipulation for module imports

--- test/test_evaluarIndividuos.py
+++ test/test_evaluarIndividuos.py
@@ -1,5 +1,9 @@
 from src.evaluarIndividuos import evaluarIndividuos
 
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
 def test_evaluar_individuos():
     poblacion = [
