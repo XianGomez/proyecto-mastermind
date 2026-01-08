a9cee3154af0d6de808bf56e86bc3f081ceebf23
XianGomez <xiiangomezz@gmail.com>
2026-01-08 09:27:08 +0100

    fix(tests): Añadir linea para que encuentren la carpeta src

--- test/test_poblarGeneracion.py
+++ test/test_poblarGeneracion.py
@@ -1,5 +1,9 @@
 from src.poblarGeneracion import poblarGeneracion
 
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
 def test_poblar_generacion_truncar():
     hijos = [[1], [2], [3], [4]]
