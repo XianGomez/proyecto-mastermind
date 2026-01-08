31e8c12116ceda80a912e6e63190f6af34cc8169
XianGomez <xiiangomezz@gmail.com>
2026-01-08 09:26:07 +0100

    fix(tests): Añadir linea para que encuentren la carpeta src

--- test/test_crearPoblacionInicial.py
+++ test/test_crearPoblacionInicial.py
@@ -1,11 +1,17 @@
-from src import crearPoblacionInicial
+import sys
+import os
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+import crearPoblacionInicial 
 
 def test_crear_poblacion_determinista(monkeypatch):
     crearPoblacionInicial.poblacionInicial = []
+    
     monkeypatch.setattr(crearPoblacionInicial.random, 'choice', lambda seq: 'RED')
 
     poblacion = crearPoblacionInicial.crearPoblacionInicial(TAMANHOPOBLACION=3)
+    
     assert len(poblacion) == 3
     for individuo in poblacion:
         assert len(individuo) == 4
