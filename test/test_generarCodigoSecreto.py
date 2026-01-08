33b0fc9320094bc4236bc6c65a481b45cd8e5944
XianGomez <xiiangomezz@gmail.com>
2026-01-08 09:27:30 +0100

    fix(tests): Añadir linea para que encuentren la carpeta src
    Added sys and os imports to manage module path.

--- test/test_generarCodigoSecreto.py
+++ test/test_generarCodigoSecreto.py
@@ -1,5 +1,9 @@
 from src import generarCodigoSecreto
 
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
 def test_generar_codigo_determinista(monkeypatch):
     monkeypatch.setattr(generarCodigoSecreto.random, 'choice', lambda seq: 'BLUE')
