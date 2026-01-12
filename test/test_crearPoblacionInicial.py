a59cf7d7d03c7e3bddb48d3966f8faca8af6871f
XianGomez <xiiangomezz@gmail.com>
2026-01-12 21:40:27 +0100

    Eliminar monkeypatch

--- test/test_crearPoblacionInicial.py
+++ test/test_crearPoblacionInicial.py
@@ -8,11 +8,9 @@ import crearPoblacionInicial
 def test_crear_poblacion_determinista(monkeypatch):
     crearPoblacionInicial.poblacionInicial = []
     
-    monkeypatch.setattr(crearPoblacionInicial.random, 'choice', lambda seq: 'RED')
 
     poblacion = crearPoblacionInicial.crearPoblacionInicial(TAMANHOPOBLACION=3)
     
     assert len(poblacion) == 3
     for individuo in poblacion:
         assert len(individuo) == 4
-        assert all(color == 'RED' for color in individuo)
